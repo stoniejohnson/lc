094cccb55ca544c38b78707d22871f9e3eb76ad9
Stonie Johnson <stoniejohnson@icloud.com>
2020-04-28 15:04:01 -0700

    finished problem 3

---
+++ topInterviewQuestionsEasy/3.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def reverse(self, nums: List[int], start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead 
+        """
+        n = len(nums)
+        k %= n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
+
+
+sol = Solution()
+
+foo = [1, 2, 3, 4, 5, 6, 7]
+
+sol.rotate(foo, 3)
