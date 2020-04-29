4e723f04d2cea1fd8efc141d9468a91126d83f14
stoniejohnson <carl@Mac-mini.local>
2020-04-29 13:45:12 -0700

    started 8

---
+++ topInterviewQuestionsEasy/8.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zeroCount = 0
+        listCopy = list(nums)
+        for val in List:
+            if val == 0:
+                
