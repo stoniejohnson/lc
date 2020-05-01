88c9661b4644874358448116f03e2080e9f813fd
stoniejohnson <carl@Mac-mini.local>
2020-05-01 00:19:20 -0700

    finished 9 optimized

--- topInterviewQuestionsEasy/9.py
+++ topInterviewQuestionsEasy/9.py
@@ -2,11 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        required = {}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        print("no solution")
+            if target - nums[i] in required:
+                return([required[target - nums[i]], i])
+            else:
+                required[nums[i]] = i
 
 s = Solution()
 
