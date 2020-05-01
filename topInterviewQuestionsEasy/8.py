ef9bc30161dd079fb82c88cbe04c879eb3ba5c77
stoniejohnson <carl@Mac-mini.local>
2020-04-30 22:15:34 -0700

    finished problem 8

--- topInterviewQuestionsEasy/8.py
+++ topInterviewQuestionsEasy/8.py
@@ -2,8 +2,18 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        zeroCount = 0
-        listCopy = list(nums)
-        for val in List:
-            if val == 0:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
+
+        while count < len(nums):
+            nums[count] = 0
+            count += 1
+
+s = Solution()
+
+s.moveZeroes([1,8,0,4,5,0,3,5,0])
+
                 
